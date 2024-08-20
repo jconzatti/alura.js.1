4807bed799fd6b7f61b4153543b302171cb01eb3
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2024-08-20 13:55:00 -0300

    lista de teclas

--- js/main.js
+++ js/main.js
@@ -1,43 +1,6 @@
-function tocarSom(){
-    document.querySelector('#som_'+pClasseDaTeclaDeSom).play();
-}
-
 function tocarSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-function tocarSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-
-function tocarSomTim(){
-    document.querySelector('#som_tecla_tim').play();
-}
-
-function tocarSomPuff(){
-    document.querySelector('#som_tecla_puff').play();
-}
-
-function tocarSomSplash(){
-    document.querySelector('#som_tecla_splash').play();
-}
-
-function tocarSomToim(){
-    document.querySelector('#som_tecla_toim').play();
-}
-
-function tocarSomPsh(){
-    document.querySelector('#som_tecla_psh').play();
-}
-
-function tocarSomTic(){
-    document.querySelector('#som_tecla_tic').play();
-}
-
-function tocarSomTom(){
-    document.querySelector('#som_tecla_tom').play();
-}
-
-document.querySelector('.tecla_pom').onclick = tocarSomPom
-document.querySelector('.tecla_clap').onclick = tocarSomClap
-document.querySelector('.tecla_tim').onclick = tocarSomTim
+const lTeclas = document.querySelectorAll('.tecla')
+lTeclas[0].onclick = tocarSomPom
