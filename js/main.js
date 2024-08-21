1dd572a52da7fcb4de9a9977aa7b4e7183ef87f5
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2024-08-20 22:24:30 -0300

    tocar alura midi com teclado numérico
    7 8 9
    4 5 6
    1 2 3

--- js/main.js
+++ js/main.js
@@ -2,13 +2,50 @@ const lTeclas = document.querySelectorAll('.tecla');
 
 function tocarSom(pIDDoAudio){
     lAudio = document.querySelector(pIDDoAudio);
+    lAudio.pause();
+    lAudio.currentTime = 0;
     lAudio.play();    
 }
 
-lTeclas.forEach(function (lTecla) {
-    const lClasseDaTecla = lTecla.classList[1];
-    const lIDDoAudio = `#som_${lClasseDaTecla}`;
-    lTecla.onclick = function () {
-        tocarSom(lIDDoAudio);
+function carregarTeclasDoAluraMidi(){
+    lTeclas.forEach(function (lTecla) {
+        const lIDDoAudio = `#som_${lTecla.classList[1]}`;
+        lTecla.onclick = function () {
+            tocarSom(lIDDoAudio);
+        }
+        lTecla.onkeydown = function (evento){
+            if (evento.code === 'Enter' || evento.code === 'NumpadEnter' || evento.code === 'Space') {
+                lTecla.classList.add('ativa')    
+            }
+        }
+        lTecla.onkeyup = function (){
+            lTecla.classList.remove('ativa')    
+        }
+    });
+}
+
+function carregarTecladoNumericoParaTocarAluraMidi(){
+    document.onkeydown = function (evento){
+        for (let i = 0; i < lTeclas.length; i++) {
+            const lValorDaTeclaNumerica = i+1
+            if (evento.code === `Numpad${lValorDaTeclaNumerica}`) {
+                const lTecla = lTeclas[obterIndiceDaTeclaDeSomPeloValorDaTeclaNumerica(lValorDaTeclaNumerica)];
+                const lIDDoAudio = `#som_${lTecla.classList[1]}`;
+                tocarSom(lIDDoAudio);
+            }
+        }
+    } 
+}
+
+function obterIndiceDaTeclaDeSomPeloValorDaTeclaNumerica(pValorDaTeclaNumerica){
+    if (pValorDaTeclaNumerica < 4) {
+        return pValorDaTeclaNumerica+5
+    } else if (pValorDaTeclaNumerica < 7) {
+        return pValorDaTeclaNumerica-1
+    } else {
+        return pValorDaTeclaNumerica-7
     }
-});
+}
+
+carregarTeclasDoAluraMidi();
+carregarTecladoNumericoParaTocarAluraMidi();
